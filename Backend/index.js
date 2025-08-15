499c23f2f6448f4186448792dbd7f8ca80361a84
LokeshOtti29 <lokeshotti456@gmail.com>
2025-08-15 20:03:33 +0530

    cors-issue

--- Backend/index.js
+++ Backend/index.js
@@ -12,11 +12,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
-app.use(express.json());
+app.use(bodyParser.json());
 app.use("/auth", authRoutes);
 app.use("/expenses", expenseRoutes);
 
